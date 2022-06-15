7b7df61e0bde5cdba412ccb47dcbd4b220c7ed6d
Bowen Fu <missing>
2022-06-16 07:57:09 +0800

    Fix functor.

--- include/hspp.h
+++ include/hspp.h
@@ -47,7 +47,7 @@ namespace detail
     template <std::size_t start, class Tuple, std::size_t... I>
     constexpr decltype(auto) subtupleImpl(Tuple &&t, std::index_sequence<I...>)
     {
-        return std::forward_as_tuple(get<start + I>(std::forward<Tuple>(t))...);
+        return std::make_tuple(get<start + I>(std::forward<Tuple>(t))...);
     }
 } // namespace detail
 
