77c5b52313f9312edd979454fdfe16f32660806d
Bowen Fu <missing>
2022-06-25 13:06:27 +0800

    Fix.

--- sample/parseExpr.cpp
+++ sample/parseExpr.cpp
@@ -1,6 +1,25 @@
 #include "parser.h"
 #include <cassert>
 
+auto expectTrue(bool x)
+{
+    if (!x)
+    {
+        throw std::runtime_error{"False in expectedTrue!"};
+    }
+}
+
+template <typename T>
+auto expectEq(T const& l, T const& r)
+{
+    if (l != r)
+    {
+        std::stringstream ss;
+        ss << l << " != " << r;
+        throw std::runtime_error{ss.str()};
+    }
+}
+
 using namespace hspp::parser;
 using namespace hspp;
 
@@ -82,28 +101,28 @@ int main()
         auto const rawResult = apply || many | digit || " 1  2 34";
         auto const& result = std::get<0>(rawResult.at(0));
         auto const expected = std::vector{1, 2, 3, 4};
-        assert(std::equal(result.begin(), result.end(), expected.begin()));
+        expectTrue(std::equal(result.begin(), result.end(), expected.begin()));
     }
 
     {
         auto const result = apply || addOp || " +  * /-";
-        assert((std::get<0>(result.at(0))| 1 | 2) == 3);
+        expectEq(std::get<0>(result.at(0))| 1 | 2, 3);
 
         auto const result2 = apply || mulOp || "   * /-";
-        assert((std::get<0>(result2.at(0))| 1 | 2) == 2);
+        expectEq(std::get<0>(result2.at(0))| 1 | 2, 2);
     }
 
     {
         auto const p = digit <chainl1> addOp;
         auto const result = runParser | p | "1 + 2";
         auto const expected = 3;
-        assert(std::get<0>(result.at(0)) == expected);
+        expectEq(std::get<0>(result.at(0)), expected);
     }
 
     {
         auto const result = apply | expr | "1 - 2 * 3 + 4";
         auto const expected = -1;
-        assert(std::get<0>(result.at(0)) == expected);
+        expectEq(std::get<0>(result.at(0)), expected);
     }
     return 0;
 }
