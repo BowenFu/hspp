e5b4d2563058ebd85f2651272a964ca36184acb8
Bowen Fu <missing>
2022-06-26 08:32:15 +0800

    single header.

--- single-header.sh
+++ single-header.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd include/hspp
-awk 1 range.h data.h typeclass.h do_notation.h parser.h > ../hspp_single_header.h
+cd develop/include
+awk 1 range.h data.h typeclass.h do_notation.h parser.h > ../../include/hspp.h
