809105711a15d726b05a4b3b94f3e9041136f6d2
Bowen Fu <BowenFu@users.noreply.github.com>
2022-08-27 21:32:14 +0800

    Monad transformer. (#79)
    * Pass build.
    
    * Pass build.
    
    * Add missing files.
    
    * Fix gcc
    
    * Fix gcc
    
    * Fix tests
    
    * Single header.
    
    * Fix tests.
    
    * Fix tests.
    
    * Skip win.
    
    * Skip win.
    
    * Skip win.
    
    Co-authored-by: Bowen Fu <missing>

---
+++ include/monadTrans.h
@@ -0,0 +1,51 @@
+
+#ifndef HSPP_MONAD_TRANS_H
+#define HSPP_MONAD_TRANS_H
+
+namespace hspp
+{
+
+/////////// MonadTrans ////////////
+// class MonadTrans t where
+//     -- | Lift a computation from the argument monad to the constructed monad.
+//     lift :: (Monad m) => m a -> t m a
+template <template<template<typename...> class, typename...> class T, typename...>
+class MonadTrans;
+
+template <template <typename...> class M, typename A>
+class MaybeT
+{
+public:
+    M<data::Maybe<A>> runMaybeT;
+};
+
+constexpr auto toMaybeT = toGFunc<1> | [](auto ma)
+{
+    return MaybeT{ma};
+};
+
+template <>
+class MonadTrans<MaybeT>
+{
+public:
+    constexpr static auto lift = toMaybeT <o> (liftM | data::just);
+};
+
+template <template <template<typename...> class, typename...> class M>
+class Lift
+{
+public:
+    template <typename Func>
+    constexpr auto operator()(Func const& func) const
+    {
+        using MTType = MonadTrans<M>;
+        return MTType::lift(func);
+    }
+};
+
+template <template <template<typename...> class, typename...> class M>
+constexpr inline auto lift = toGFunc<1>(Lift<M>{});
+
+} // namespace hspp
+
+#endif // HSPP_MONAD_TRANS_H
--- sample/pause.cpp
+++ sample/pause.cpp
@@ -1,9 +1,11 @@
 #include "hspp.h"
+#include "monadTrans.h"
 #include <memory>
 #include <variant>
 #include <cassert>
 
-#if !defined(FOR_CLANG)
+// FIXME : fix build on msvc
+#if !defined(FOR_CLANG) && !defined(FOR_MSVC)
 auto expectTrue(bool x)
 {
     if (!x)
@@ -30,97 +32,180 @@ using namespace std::literals;
 
 // Based on haskell version at https://www.schoolofhaskell.com/school/to-infinity-and-beyond/pick-of-the-week/coroutines-for-streaming/part-1-pause-and-resume
 
-// data Pause m
-//   = Run (m (Pause m))
-//   | Done
+// data PauseT m r
+//   = RunT (m (PauseT m r))
+//   | DoneT r
 
-template <template <typename...> class M>
-struct Run;
+template <template <typename...> class M, typename R>
+struct RunT;
 
-class Done{};
-
-template <template <typename...> class M>
-using PauseBase = std::variant<Run<M>, Done>;
-
-template <template <typename...> class M>
-class Pause : public PauseBase<M>
+template <typename R>
+class DoneT
 {
 public:
-    using PauseBase<M>::PauseBase;
+    R r;
 };
 
-template <template <typename...> class M>
-using PausePtr = std::shared_ptr<Pause<M>>;
+template <template <typename...> class M, typename R>
+using PauseTBase = std::variant<RunT<M, R>, DoneT<R>>;
 
-template <template <typename...> class M>
-struct Run
+template <template <typename...> class M, typename R>
+class PauseT : public PauseTBase<M, R>
 {
-    M<PausePtr<M>> data;
+public:
+    using PauseTBase<M, R>::PauseTBase;
 };
 
-template <template <typename...> class M>
-const auto done = std::make_shared<Pause<M>>(Done{});
-
-template <template <typename...> class M, typename... Ts>
-constexpr auto toRunImpl(M<PausePtr<M>, Ts...> d) -> PausePtr<M>
+template <template <typename...> class M, typename R>
+class PauseTPtr : public std::shared_ptr<PauseT<M, R>>
 {
-    return std::make_shared<Pause<M>>(Run<M>{d});
+public:
+    PauseTPtr(std::shared_ptr<PauseT<M, R>> ptr)
+    : std::shared_ptr<PauseT<M, R>>{std::move(ptr)}
+    {}
+};
+
+template <template <typename...> class M, typename R>
+struct RunT
+{
+    M<PauseTPtr<M, R>> data;
+};
+
+template <template <typename...> class M, typename R, typename... Ts>
+constexpr auto toDoneTPtrImpl(R r) -> PauseTPtr<M, R>
+{
+    return std::make_shared<PauseT<M, R>>(DoneT<R>{r});
 }
 
-constexpr auto toRun = toGFunc<1> | [](auto d)
+template <template <typename...> class M>
+constexpr auto toDoneTPtr = toGFunc<1> | [](auto d)
 {
-    return toRunImpl(d);
+    return toDoneTPtrImpl<M>(d);
 };
 
-const auto pauseExample = toRun || toTEIO | do_(
-  putStrLn | "Let's begin",
-  putStrLn | "Step 1",
-  return_ || (toRun || toTEIO | do_(
-    putStrLn | "Step 2",
-    return_ || (toRun || toTEIO | do_(
-      putStrLn | "Step 3",
-      putStrLn | "Yay, we're done!",
-      return_ | done<IO>
-    ))
-  ))
+template <template <typename...> class M, typename R, typename... Ts>
+constexpr auto toRunPtrImpl(M<PauseTPtr<M, R>, Ts...> d) -> PauseTPtr<M, R>
+{
+    return std::make_shared<PauseT<M, R>>(RunT<M, R>{d});
+}
+
+constexpr auto toRunTPtr = toGFunc<1> | [](auto d)
+{
+    return toRunPtrImpl(d);
+};
+
+template <typename R>
+using PausePtrIO = PauseTPtr<IO, R>;
+
+namespace hspp
+{
+
+class ApplicativePausePtr
+{
+public:
+    constexpr static auto pure = toGFunc<1> | [](auto x)
+    {
+        return DoneT<decltype(x)>(x);
+    };
+};
+
+template <>
+class Applicative<PausePtrIO> : public ApplicativePausePtr
+{};
+
+template <template <typename...> class M>
+class MonadBasePause
+{
+public:
+    template <typename R, typename Func>
+    constexpr static auto bind(PauseTPtr<M, R> const& t1, Func const& func) -> std::invoke_result_t<Func, R>
+    {
+        using RetT = std::invoke_result_t<Func, R>;
+        return std::visit(
+            overload(
+                [&](DoneT<R> const& r) -> RetT
+                {
+                    return func(r.r);
+                },
+                [&](RunT<M, R> const& r) -> RetT
+                {
+                    return toRunTPtr(
+                        liftM | [=](PauseTPtr<M, R> const& v) { return bind(v, func); } | r.data
+                    );
+                })
+            , static_cast<PauseTBase<M, R> const&>(*t1));
+    }
+};
+
+template <>
+class MonadBase<PausePtrIO> : public MonadBasePause<IO>
+{};
+
+template <>
+class MonadTrans<PauseTPtr>
+{
+public:
+    // use IO for now.
+    constexpr static auto lift = toRunTPtr <o> (liftM | toDoneTPtr<IO>);
+};
+
+template <template <template<typename...> typename Type, typename... Ts> class TypeClassT, typename R>
+struct TypeClassTrait<TypeClassT, PausePtrIO<R>>
+{
+    using Type = TypeClassT<PausePtrIO>;
+};
+
+
+} // namespace hspp
+
+// pause :: Monad m => PauseT m ()
+// pause = DoneT ()
+const auto pause = toDoneTPtr<IO>(_o_);
+
+const auto example2 = do_(
+  lift<PauseTPtr> || putStrLn | "Step 1",
+  pause,
+  lift<PauseTPtr> || putStrLn | "Step 2",
+  pause,
+  (lift<PauseTPtr> || putStrLn | "Step 3")
 );
 
-template <template <typename...> class M>
-constexpr auto runNImpl(int n, PausePtr<M> p) -> M<PausePtr<M>>;
+template <template <typename...> class M, typename R>
+constexpr auto runNTImpl(int n, PauseTPtr<M, R> p) -> M<PauseTPtr<M, R>>;
 
-constexpr auto runN = toGFunc<2> | [](int n, auto p)
+constexpr auto runNT = toGFunc<2> | [](int n, auto p)
 {
-    return runNImpl(n, p);
+    return runNTImpl(n, p);
 };
 
-template <template <typename...> class M>
-constexpr auto runNImpl(int n, PausePtr<M> p) -> M<PausePtr<M>>
+template <template <typename...> class M, typename R>
+constexpr auto runNTImpl(int n, PauseTPtr<M, R> p) -> M<PauseTPtr<M, R>>
 {
-    return toTEIO | io([=]() -> PausePtr<M>
+    return toTEIO | io([=]() -> PauseTPtr<M, R>
     {
         if (n == 0)
         {
             return p;
         }
-        if (p == done<IO>)
+        if (std::get_if<DoneT<R>>(p.get()))
         {
-            return done<IO>;
+            return p;
         }
         assert (n >= 0);
-        return (std::get<Run<M>>(*p).data >>= ([n](auto p){ return runNImpl(n-1, p); })).run();
+        return (std::get<RunT<M, R>>(*p).data >>= ([n](auto p){ return runNTImpl(n-1, p); })).run();
     });
 }
 
-template <template <typename...> class M>
-constexpr auto fullRunImpl(PausePtr<M> p) -> M<_O_>
+template <template <typename...> class M, typename R>
+constexpr auto fullRunImpl(PauseTPtr<M, R> p) -> M<_O_>
 {
     return toTEIO | io([=]() -> _O_
     {
-        if (p == done<IO>)
+        if (auto ptr = std::get_if<DoneT<R>>(p.get()))
         {
-            return _o_;
+            return ptr->r;
         }
-        return (std::get<Run<M>>(*p).data >>= ([](auto p){ return fullRunImpl(p); })).run();
+        return (std::get<RunT<M, R>>(*p).data >>= ([](auto p){ return fullRunImpl(p); })).run();
     });
 }
 
@@ -131,15 +216,17 @@ constexpr auto fullRun = toGFunc<1> | [](auto p)
 
 int main()
 {
-    Id<PausePtr<IO>> rest;
+    static_cast<void>(Applicative<PausePtrIO>::pure);
+    static_cast<void>(MonadTrans<PauseTPtr>::lift);
+    Id<PauseTPtr<IO, _O_>> rest;
     auto main_ = do_(
-        rest <= (runN | 2 | pauseExample),
+        rest <= (runNT | 2 | example2),
         putStrLn | "=== should print through step 2 ===",
-        runN | 1 | rest,
+        runNT | 1 | rest,
         // remember, IO Foo is just a recipe for Foo, not a Foo itself
         // so we can run that recipe again
         fullRun | rest,
-        fullRun | pauseExample
+        fullRun | example2
     );
     main_.run();
 
@@ -151,4 +238,4 @@ int main()
 {
     return 0;
 }
-#endif // !defined(FOR_CLANG)
+#endif // !defined(FOR_CLANG) && !defined(FOR_MSVC)
