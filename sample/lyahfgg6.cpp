89f3acaf576811b14ab61c8779b36d6bc52c86f4
Bowen Fu <missing>
2022-09-24 11:23:11 +0800

    Fix build.

--- sample/lyahfgg6.cpp
+++ sample/lyahfgg6.cpp
@@ -121,15 +121,15 @@ void someHigherOrderismIsInOrder1()
 {
 #if 0
     // haskell version
-    ghci> zipWith' (+) [4,2,5,6] [2,6,2,3]
+    ghci> zipWith (+) [4,2,5,6] [2,6,2,3]
     [6,8,7,9]
-    ghci> zipWith' max [6,3,2,1] [7,3,1,5]
+    ghci> zipWith max [6,3,2,1] [7,3,1,5]
     [7,3,2,5]
-    ghci> zipWith' (++) ["foo ", "bar ", "baz "] ["fighters", "hoppers", "aldrin"]
+    ghci> zipWith (++) ["foo ", "bar ", "baz "] ["fighters", "hoppers", "aldrin"]
     ["foo fighters","bar hoppers","baz aldrin"]
-    ghci> zipWith' (*) (replicate 5 2) [1..]
+    ghci> zipWith (*) (replicate 5 2) [1..]
     [2,4,6,8,10]
-    ghci> zipWith' (zipWith' (*)) [[1,2,3],[3,5,6],[2,3,4]] [[3,2,2],[3,4,5],[5,4,3]]
+    ghci> zipWith (zipWith (*)) [[1,2,3],[3,5,6],[2,3,4]] [[3,2,2],[3,4,5],[5,4,3]]
     [[3,4,6],[9,20,30],[10,12,12]]
 #endif // 0
 
